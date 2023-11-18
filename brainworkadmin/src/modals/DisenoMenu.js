835512e15ccb6a1392c6ee64026631380135442f
Angel Montalvo <angelmontalvo854@gmail.com>
2023-11-18 13:03:35 -0600

    Menú principal terminado
    Se termino de implementar el menú principal, con tres secciones que redirigen a sus modulos correspondientes y un sistema de notificaciones

--- brainworkadmin/src/modals/DisenoMenu.js
+++ brainworkadmin/src/modals/DisenoMenu.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FaBell, FaEnvelope} from 'react-icons/fa';
+import { FaBell} from 'react-icons/fa';
 import './styles.css';
+import { Link } from 'react-router-dom';
+import Notificaciones from './NotificacionesModal';
 
 
 const DisenoMenu = () => {
@@ -12,81 +14,80 @@ const DisenoMenu = () => {
     
     <div>
 
-      <div style={{ display: 'flex', padding: '30px' }}>
-        <h3 className='notificaciones'>Notificaciones</h3>
-        <FaBell style={{ fontSize: '38px', color: 'Blue', cursor: 'pointer'}} />
+      <div className='notificaciones'>
+        <Notificaciones></Notificaciones>
       </div>
 
         {/* Sección de Tareas */}
         <div className="menu-section">
           <h2 className='color'>Tareas Asignadas</h2>
-
+          <Link to="/tareas" className='enlaces'>
           <div className="task-subsection">
             <div className="task-section">
-              <h4>Sección 1</h4>
-              <p>Contenido de la sección 1</p>
+              <h4>Programación I</h4>
+              <p>FrontEnd: Crear una aplicación usando ReactJs</p>
             </div>
             {/* Sección 2 */}
-            <div className="task-section">
-              <h4>Sección 2</h4>
-              <p>Contenido de la sección 2</p>
+            <div className="task-section seleccion">
+              <h4>Programación I</h4>
+              <p>Base de datos: Se debe crear una base de datos</p>
             </div>
             {/* Sección 3 */}
             <div className="task-section">
-              <h4>Sección 3</h4>
-              <p>Contenido de la sección 3</p>
+              <h4>Base de datos I</h4>
+              <p>Subir registros: Subir los registros a la base de datos mediante el sistema</p>
             </div>
             {/* Sección 4 */}
             <div className="task-section">
-              <h4>Sección 4</h4>
-              <p>Contenido de la sección 4</p>
+              <h4>Progrmación I</h4>
+              <p>Conectar API: Realizar la conexión con la API</p>
             </div>
 
           </div>
-          {/* Botón al final del div "Hacer Esto" */}
-          <button className="btn btn-primary float-end">Abrir Tareas</button>
+          </Link>
         </div>
 
+
         {/* Sección de Calendario */}
+        
         <div className="menu-section">
           <h2 className='color'>Calendario</h2>
           <div className="calendar-section">
-            <p>{date.toDateString()}</p>
-            <Calendar onChange={setDate} value={date} />
+            <Link to="/calendario" className='enlaces'>
+              <p>{date.toDateString()}</p>
+              <Calendar onChange={setDate} value={date} />
+            </Link>
           </div>
-          {/* Botón al final del div de Calendario */}
-          <button className="btn btn-primary float-end">Abrir Calendario</button>
         </div>
 
         {/* Sección de Recordatorio */}
         <div className="menu-section">
           <h2 className='color'>Recordatorio</h2>
+          <Link to="/recordatorio" className='enlaces'>
           <div className="reminder-subsection">
-
             {/* Sección 1 */}
-            <div className="reminder-section">
-              <h4>Recordatorio 1</h4>
-              <p>Contenido del recordatorio 1</p>
+            <div className="reminder-section pendiente">
+              <h4 className='text-center'>FrontEnd</h4>
+              <p>Pendiente</p>
             </div>
             {/* Sección 2 */}
-            <div className="reminder-section">
-              <h4>Recordatorio 2</h4>
-              <p>Contenido del recordatorio 2</p>
+            <div className="reminder-section proceso">
+              <h4 className='text-center'>Base de datos</h4>
+              <p>En proceso</p>
             </div>
             {/* Sección 3 */}
-            <div className="reminder-section">
-              <h4>Recordatorio 3</h4>
-              <p>Contenido del recordatorio 3</p>
+            <div className="reminder-section terminado">
+              <h4 className='text-center'>Subir archivos</h4>
+              <p>Terminado</p>
             </div>
             {/* Sección 4 */}
-            <div className="reminder-section">
-              <h4>Recordatorio 4</h4>
-              <p>Contenido del recordatorio 4</p>
+            <div className="reminder-section pendiente">
+              <h4 className='text-center'>Conectar API</h4>
+              <p>Pendiente</p>
             </div>
 
           </div>
-          {/* Botón al final del div "Recordatorio" */}
-          <button className="btn btn-primary float-end">Abrir Recordatorio</button>
+          </Link>
         </div>
 
     </div>
